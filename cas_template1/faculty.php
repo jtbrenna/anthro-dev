4f259a38e16646d84ff90cbec8ab304a0e45844f
Jack Brennan <jtbrenna@uvm.edu>
2014-03-25 22:07:06 -0400

    more faculty work

--- cas_template1/faculty.php
+++ cas_template1/faculty.php
@@ -4,29 +4,33 @@
     }
 
     .faculty img{
-        display: block;
+        display: inline-block;
         margin-top: 1%;
         margin-bottom: 1%;
+    }
+
+    .faculty h4{
+        display: inline;
     }
 
 </style>
 
 <h3>Faculty - First Last</h3>
 
-<div class="faculty"><img src="images/blom.jpg"></div>
-<div class="faculty"><img src="images/crock.jpg"></div>
-<div class="faculty"><img src="images/dickinson.jpg"></div>
-<div class="faculty"><img src="images/eastman.jpg"></div>
-<div class="faculty"><img src="images/gordon.jpg"></div>
-<div class="faculty"><img src="images/manetta.jpg"></div>
-<div class="faculty"><img src="images/mares.jpg"></div>
-<div class="faculty"><img src="images/shea.jpg"></div>
-<div class="faculty"><img src="images/steinberg.jpg"></div>
-<div class="faculty"><img src="images/vankeuren.jpg"></div>
-<div class="faculty"><img src="images/vanvalkenburgh.jpg"></div>
-<div class="faculty"><img src="images/vivanco.jpg"></div>
+<div class="faculty"><img src="images/blom.jpg"><h4>Deborah Blom</h4></div>
+<div class="faculty"><img src="images/crock.jpg"><h4>John Crock</h4></div>
+<div class="faculty"><img src="images/dickinson.jpg"><h4>Jennifer Dickinson</h4></div>
+<div class="faculty"><img src="images/eastman.jpg"><h4>Benjamin Eastman</h4></div>
+<div class="faculty"><img src="images/gordon.jpg"><h4>Robert Gordon</h4></div>
+<div class="faculty"><img src="images/manetta.jpg"><h4>Emily Manetta</h4></div>
+<div class="faculty"><img src="images/mares.jpg"><h4>Teresa Mares</h4></div>
+<div class="faculty"><img src="images/shea.jpg"><h4>Jeanne Shea</h4></div>
+<div class="faculty"><img src="images/steinberg.jpg"><h4>Jonah Steinberg</h4></div>
+<div class="faculty"><img src="images/vankeuren.jpg"><h4>Scott Van Keuren</h4></div>
+<div class="faculty"><img src="images/vanvalkenburgh.jpg"><h4>Parker VanValkenburgh</h4></div>
+<div class="faculty"><img src="images/vivanco.jpg"><h4>Luis Vivanco</h4></div>
 
 <h2>Lecturers</h2>
 
-<div class="faculty"><img src="images/matter.jpg"></div>
-<div class="faculty"><img src="images/robinson.jpg"></div>
+<div class="faculty"><img src="images/matter.jpg"><h4>Scott Matter</h4></div>
+<div class="faculty"><img src="images/robinson.jpg"><h4>Francis "Jess" Robinson</h4></div>
