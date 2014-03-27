fc56154ab514156955f8f8166835443a60dc31a3
Jack Brennan <jtbrenna@uvm.edu>
2014-03-27 16:49:19 -0400

    faculty look

--- cas_template1/faculty.php
+++ cas_template1/faculty.php
@@ -11,26 +11,47 @@
 
     .faculty h4{
         display: inline;
+        font-size: larger;
+    }
+
+    .faculty div{
+        display: inline;
+        position: absolute;
+        width: auto;
+        margin-left: 5%;
+        margin-top: 10%;
+    }
+
+    .faculty-list div.faculty:nth-child(even) {
+        background-color: #EBF7FF;
+    }
+
+    .lecturer-list div.faculty:nth-child(even) {
+        background-color: #EBF7FF;
     }
 
 </style>
 
 <h3>Faculty - First Last</h3>
 
-<div class="faculty"><img src="images/blom.jpg"><h4>Deborah Blom</h4></div>
-<div class="faculty"><img src="images/crock.jpg"><h4>John Crock</h4></div>
-<div class="faculty"><img src="images/dickinson.jpg"><h4>Jennifer Dickinson</h4></div>
-<div class="faculty"><img src="images/eastman.jpg"><h4>Benjamin Eastman</h4></div>
-<div class="faculty"><img src="images/gordon.jpg"><h4>Robert Gordon</h4></div>
-<div class="faculty"><img src="images/manetta.jpg"><h4>Emily Manetta</h4></div>
-<div class="faculty"><img src="images/mares.jpg"><h4>Teresa Mares</h4></div>
-<div class="faculty"><img src="images/shea.jpg"><h4>Jeanne Shea</h4></div>
-<div class="faculty"><img src="images/steinberg.jpg"><h4>Jonah Steinberg</h4></div>
-<div class="faculty"><img src="images/vankeuren.jpg"><h4>Scott Van Keuren</h4></div>
-<div class="faculty"><img src="images/vanvalkenburgh.jpg"><h4>Parker VanValkenburgh</h4></div>
-<div class="faculty"><img src="images/vivanco.jpg"><h4>Luis Vivanco</h4></div>
+<div class="faculty-list">
+<div class="faculty"><img src="images/blom.jpg"><div><h4>Deborah Blom</h4></div></div>
+<div class="faculty"><img src="images/crock.jpg"><div><h4>John Crock</h4></div></div>
+<div class="faculty"><img src="images/dickinson.jpg"><div><h4>Jennifer Dickinson</h4></div></div>
+<div class="faculty"><img src="images/eastman.jpg"><div style="margin-top:8%;"><h4>Benjamin Eastman</h4></div></div>
+<div class="faculty"><img src="images/gordon.jpg"><div><h4>Robert Gordon</h4></div></div>
+<div class="faculty"><img src="images/manetta.jpg"><div><h4>Emily Manetta</h4></div></div>
+<div class="faculty"><img src="images/mares.jpg"><div><h4>Teresa Mares</h4></div></div>
+<div class="faculty"><img src="images/shea.jpg"><div><h4>Jeanne Shea</h4></div></div>
+<div class="faculty"><img src="images/steinberg.jpg"><div><h4>Jonah Steinberg</h4></div></div>
+<div class="faculty"><img src="images/vankeuren.jpg"><div><h4>Scott Van Keuren</h4></div></div>
+<div class="faculty"><img src="images/vanvalkenburgh.jpg"><div><h4>Parker VanValkenburgh</h4></div></div>
+<div class="faculty"><img src="images/vivanco.jpg"><div><h4>Luis Vivanco</h4></div></div>
+</div>
 
 <h2>Lecturers</h2>
 
-<div class="faculty"><img src="images/matter.jpg"><h4>Scott Matter</h4></div>
-<div class="faculty"><img src="images/robinson.jpg"><h4>Francis "Jess" Robinson</h4></div>
+<div class="lecturer-list">
+<div class="faculty"><img src="images/matter.jpg"><div><h4>Scott Matter</h4></div></div>
+<div class="faculty"><img src="images/robinson.jpg"><div style="margin-top:7%;"><h4>Francis "Jess" Robinson</h4></div></div>
+</div>
